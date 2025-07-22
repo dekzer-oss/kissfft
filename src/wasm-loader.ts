a530771ab4abe7758460fd4474e212f196248bcd
Maikel Eckelboom <maikeleckelboom@outlook.com>
2025-07-22 11:33:30 +0200

    chore: update invalid test structure and wasm-loader

--- src/wasm-loader.ts
+++ src/wasm-loader.ts
@@ -4,7 +4,7 @@ import type { KissFftWasmModule } from './types';
 let modulePromise: Promise<KissFftWasmModule>;
 
 /**
- * Loads the kissfft-wasm module.
+ * Loads the dekzer-kissfft module.
  * @returns Promise that resolves to the KissFftWasmModule instance.
  * This function ensures that the module is loaded only once,
  * even if called multiple times.
@@ -14,7 +14,7 @@ export function loadKissFft(): Promise<KissFftWasmModule> {
     const spec = new URL('../build/kissfft-wasm.js', import.meta.url).href;
     modulePromise = import(spec).then(({ default: factory }: any) => {
       if (typeof factory !== 'function') {
-        throw new Error('kissfft-wasm: default export is not a factory');
+        throw new Error('kissfft-kissfft: default export is not a factory');
       }
       return factory() as Promise<KissFftWasmModule>;
     });
